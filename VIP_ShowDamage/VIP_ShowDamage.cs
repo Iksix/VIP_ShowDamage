67c950a835e18abd22f9a7b2637a1d5d0dd65f73
iksixx <pocetikyt@gmail.com>
2024-08-07 13:33:21 +0700

    1

--- VIP_ShowDamage/VIP_ShowDamage.cs
+++ VIP_ShowDamage/VIP_ShowDamage.cs
@@ -1,46 +1,129 @@
 ﻿using System.Runtime.InteropServices;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Core.Capabilities;
 using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Modules.Utils;
-using Modularity;
+using Microsoft.Extensions.Localization;
 using VipCoreApi;
+using static VipCoreApi.IVipCoreApi;
 
 namespace VIP_ShowDamage;
 
-public class VipShowDamage : BasePlugin, IModulePlugin
+public class VipShowDamage : BasePlugin
 {
     public override string ModuleAuthor => "iks";
     public override string ModuleName => "[VIP] ShowDamage";
     public override string ModuleVersion => "v1.0.2";
     
-    private static readonly string Feature = "ShowDamage";
-    private IVipCoreApi _api = null!;
+    private IVipCoreApi? _api = null!;
+    private ShowDamage? _showDamage = null!;
     
-    public void LoadModule(IApiProvider provider)
-    {
-        _api = provider.Get<IVipCoreApi>();
-        _api.RegisterFeature(Feature);
-    }
-    
-    public override void Load(bool hotReload)
-    {
-        RegisterEventHandler<EventPlayerHurt>((@event, info) =>
-        {
-            var attacker = @event.Attacker;
-            if (!attacker.IsValid) return HookResult.Continue;
-            if (attacker.PlayerName == @event.Userid.PlayerName) return HookResult.Continue;
+    private PluginCapability<IVipCoreApi> PluginCapability { get; } = new("vipcore:core");
 
-            if (!_api.IsClientVip(attacker)) return HookResult.Continue;
-            if (!_api.PlayerHasFeature(attacker, Feature)) return HookResult.Continue;
-            if (_api.GetPlayerFeatureState(attacker, Feature) is IVipCoreApi.FeatureState.Disabled
-                or IVipCoreApi.FeatureState.NoAccess) return HookResult.Continue;
-            if(!_api.GetFeatureValue<bool>(attacker, Feature)) return HookResult.Continue;
-            
-            attacker.PrintToCenterHtml($" Нанесён урон: <font color='red'>{@event.DmgHealth}HP</font>");
-            
-            return HookResult.Continue;
-        });
+    
+
+    public override void OnAllPluginsLoaded(bool hotReload)
+    {
+        _api = PluginCapability.Get();
+        if (_api == null) return;
+
+        _api.OnCoreReady += () =>
+        {
+            _showDamage = new ShowDamage(_api, Localizer);
+
+            RegisterEventHandler<EventPlayerHurt>(_showDamage.OnPlayerHurt);
+            RegisterListener<Listeners.OnTick>(() => {
+                _showDamage.ShowDamageMessage();
+            });
+
+            _api.RegisterFeature(_showDamage);
+        };
     }
+
+    public override void Unload(bool hotReload)
+    {
+        _api?.UnRegisterFeature(_showDamage);
+    }
+}
+
+public class ShowDamage : VipFeatureBase
+{
+    public override string Feature => "ShowDamage";
+    IStringLocalizer Localizer;
+
+    public Dictionary<CCSPlayerController, string> messages = new();
+    public Dictionary<CCSPlayerController, CounterStrikeSharp.API.Modules.Timers.Timer> deleteTimers = new();
+    CounterStrikeSharp.API.Modules.Timers.Timer timer;
+
+
+    public ShowDamage(IVipCoreApi api, IStringLocalizer localizer) : base(api)
+    {
+        Localizer = localizer;
+        this.timer = timer;
+    }
+
+    public void ShowDamageMessage()
+    {
+        foreach (var msg in messages)
+        {
+            PrintHtml(msg.Key, msg.Value);
+        }
+    }
+
+    public void PrintHtml(CCSPlayerController player, string hudContent) // thn for deafps
+        {
+            var @event = new EventShowSurvivalRespawnStatus(false)
+            {
+                LocToken = hudContent,
+                Duration = 5,
+                Userid = player
+            };
+            @event.FireEvent(false);
+
+            @event = null;
+        }
+
+
+    [GameEventHandler]
+    public HookResult OnPlayerHurt(EventPlayerHurt @event, GameEventInfo info)
+    {
+        var attacker = @event.Attacker;
+        var target = @event.Userid;
+
+        if (attacker == null || !attacker.IsValid || attacker.IsBot || attacker == target) return HookResult.Continue;
+
+        if (!PlayerHasFeature(attacker)) return HookResult.Continue;
+
+        if (GetPlayerFeatureState(attacker) != IVipCoreApi.FeatureState.Enabled) return HookResult.Continue;
+
+        if (attacker.TeamNum == target.TeamNum) return HookResult.Continue;
+        
+        int Damage = @event.DmgHealth;
+
+        string message = Localizer["ShowDamageMsg"].ToString()
+        .Replace("{name}", target.PlayerName)
+        .Replace("{damage}", Damage.ToString())
+        ;
+
+        if (deleteTimers.ContainsKey(attacker))
+        {
+            deleteTimers[attacker].Kill();
+            deleteTimers.Remove(attacker);
+            messages.Remove(attacker);
+        }
+
+        messages.Add(attacker, message);
+
+        deleteTimers.Add(attacker, new CounterStrikeSharp.API.Modules.Timers.Timer(1, () => {
+            messages.Remove(attacker);
+        }));
+
+
+        return HookResult.Continue;
+    }
+    
 }
